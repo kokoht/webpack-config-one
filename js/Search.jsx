711f2744b9b164b46d143ca0f1c8a844fa63e16c
kokoh <kokohtan@gmail.com>
2018-07-04 17:17:11 +0700

    filtering till hmr

--- js/Search.jsx
+++ js/Search.jsx
@@ -13,7 +13,7 @@ class Search extends Component {
     return (
       <div className="search">
         <header>
-          <h1>svideo</h1>
+          <h1>- netflux -</h1>
           <input
             onChange={this.handleSearchTermChange}
             value={this.state.searchTerm}
@@ -22,9 +22,14 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows.map(show => (
-            <ShowCard key={show.imdbID} show={show} />
-          ))}
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} show={show} />)}
         </div>
       </div>
     );
